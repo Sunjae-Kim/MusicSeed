ce64c09a7b6ffdb1a97b7c551993a47ec27f2d3c
kimsj9484 <kimsj9484@gmail.com>
2018-11-24 01:10:43 +0900

    Add barPlayer

--- musicseed_react/src/components/BarPlayer.js
+++ musicseed_react/src/components/BarPlayer.js
@@ -1,12 +1,46 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import "../styles/BarPlayer.css";
+import MediaButtons from "./MediaButtons";
 
-const BarPlayer = (props) => {
+const BarPlayer = ({song}) => {
   return(
-    <div className={'barplayer'}>
-
+    <div>
+      { renderSong(song) }
     </div>
   )
-}
+};
 
-export default BarPlayer;
+const renderSong = song => {
+  if(!song){
+    return(
+      <div className={'barplayer container'}>
+      </div>
+    )
+  } else{
+    return(
+      <div className={'barplayer container'}>
+        <div className={'ui grid'}>
+          <div id="barPlayerRow" className="row">
+            <div className="ten wide column">
+              <div className="content">
+                <h3>{ song.title }</h3>
+              </div>
+              <div className="right float content">
+                <h4>{ song.artist }</h4>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
+
+const mapStateToProps = state => {
+  return {
+    song : state.playedSong
+  }
+};
+
+export default connect(mapStateToProps)(BarPlayer);;
