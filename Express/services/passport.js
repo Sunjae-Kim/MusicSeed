581b71081920ec6ed60bdf1c2682e8d8e9e6077f
laggu <kwla103@naver.com>
2018-11-28 09:24:50 +0900

    add google-oauth On branch login Changes to be committed: 	modified:   Express/app.js 	new file:   Express/routes/auth.js 	modified:   Express/routes/users.js 	new file:   Express/services/passport.js

---
+++ Express/services/passport.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+//const { User } = require('../models/User');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id); // set-cookie(serialized(user.id))
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id)
+        .then(user => {
+            done(null, user)
+        })
+        .catch(error => console.error(error.message));
+});
+
+passport.use(new GoogleStrategy(
+    {
+        clientID: "568719346323-hnjqt6utbqave1q6bcnq3374r09pqbov.apps.googleusercontent.com",
+        clientSecret: "8TwKvrm5pW_I3fx7beBkqB7S",
+        callbackURL: "/auth/google/callback",
+        proxy: true
+    },
+    (accessToken, refreshToken, profile, done) => {
+        // let user = User.findOne({ googleID: profile.id })
+        //     .then(existingUser => {
+        //         if(existingUser) { // User exists
+        //             done(null, existingUser);
+        //         } else { // New user
+        //             new User({ googleID: profile.id })
+        //                 .save()
+        //                 .then(newUser => done(null, newUser))
+        //                 .catch(error => console.error(error.message));
+        //         }
+        //     })
+        //     .catch(error => console.error(error.message));
+        console.log(profile);
+        done();
+    }
+));
