684d553fe391fad85977fe7f22f18d482ba642cd
kimsj9484 <kimsj9484@gmail.com>
2018-12-13 17:17:51 +0900

    Add statics and method on user model

--- Express/models/user.js
+++ Express/models/user.js
@@ -11,26 +11,55 @@ const user_schema = new mongoose.Schema({
   albums: [{ type: mongoose.Schema.Types.ObjectId, ref: "Album" }],
   playlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Music" }],
   comment: [{ type: mongoose.Schema.Types.ObjectId, ref: "Comment" }],
-    oauth: Boolean,
+  oauth: Boolean,
+  admin: {type: Boolean, default: false},
 });
-
-const User = mongoose.model("User", user_schema);
 
 function validate_user(user) {
   const schema = {
     email: Joi.string().required(),
     pw: Joi.string(),
-    name: Joi.string().required().lowercase(),
+    name: Joi.string()
+      .required()
+      .lowercase(),
     nickname: Joi.string().lowercase(),
     seed: Joi.number().default(0),
     description: Joi.string(),
     albums: Joi.array(),
     playlist: Joi.array(),
     comment: Joi.array(),
-      oauth: Joi.boolean(),
+    oauth: Joi.boolean(),
+    admin: Joi.boolean(),
   };
   return Joi.validate(user, schema);
 }
+
+// create new User document
+user_schema.statics.create = function(newUSer) {
+  const user = new this(newUSer);
+
+  // return the Promise
+  return user.save();
+};
+
+// find one user by using email
+user_schema.statics.findOneByUsername = function(email) {
+  return this.findOne({
+    email
+  }).exec();
+};
+
+// verify the password of the User documment
+user_schema.methods.verify = function(pw) {
+  return this.pw === pw;
+};
+
+user_schema.methods.assignAdmin = function() {
+  this.admin = true;
+  return this.save();
+};
+
+const User = mongoose.model("User", user_schema);
 
 exports.User = User;
 exports.validate = validate_user;
