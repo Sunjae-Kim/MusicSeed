f24d3352ba06a2b178009c5b7a70e92b5005b5df
kimsj9484 <kimsj9484@gmail.com>
2018-12-20 11:31:33 +0900

    Remove previous local oauth logic

--- Express/musicseed_react/src/pages/Login/Login.js
+++ Express/musicseed_react/src/pages/Login/Login.js
@@ -1,66 +1,66 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { setPath, checkEmail, checkPassword } from "../../actions/index";
-import '../../styles/Login.css';
+import "../../styles/Login.css";
 import axios from "axios";
 
 class Login extends Component {
-    onFormSubmit = (event) => {
-        event.preventDefault();
-        let user = {
-            email: this.props.checkedEmail,
-            pw: this.props.checkedPassword,
-        };
-        axios.post('/auth/login', user)
-            .then(function (response) {
-            if (response.data.redirect === '/') {
-                window.location = "/";
-            } else{
-                window.location = '/login';
-            }
-        });
-    };
+  onFormSubmit = event => {
+    event.preventDefault();
+  };
 
   render() {
     return (
-      <form className="login ui form" onSubmit={e => this.onFormSubmit(e) }>
+      <form className="login ui form" onSubmit={e => this.onFormSubmit(e)}>
         <div className="field">
           <h1>Login</h1>
           <input
             type="email"
             placeholder="Email"
-            onChange={ e => this.props.checkEmail(e.target.value) }
-            value={ this.props.checkedEmail }
+            onChange={e => this.props.checkEmail(e.target.value)}
+            value={this.props.checkedEmail}
           />
         </div>
         <div className="field">
           <input
             type="password"
             placeholder="Password"
-            onChange={ e => this.props.checkPassword(e.target.value) }
-            value={ this.props.checkedPassword }
+            onChange={e => this.props.checkPassword(e.target.value)}
+            value={this.props.checkedPassword}
           />
         </div>
-        <button className="ui button" type="submit">LOGIN</button>
-        <hr/>
-        <p>Music Seed 회원이 아닌가요? <strong><Link to={'/register'} className={'a'} onClick={ () => this.props.setPath('register')} >	&nbsp; 지금 가입하세요.</Link></strong></p>
-          <a href="/auth/google"> 구글로그인 </a>
+        <button className="ui button" type="submit">
+          LOGIN
+        </button>
+        <hr />
+        <p>
+          Music Seed 회원이 아닌가요?{" "}
+          <strong>
+            <Link
+              to={"/register"}
+              className={"a"}
+              onClick={() => this.props.setPath("register")}
+            >
+              {" "}
+              &nbsp; 지금 가입하세요.
+            </Link>
+          </strong>
+        </p>
+        <a href="/api/auth/google"> 구글로그인 </a>
       </form>
     );
   }
 }
 
-
-
 const mapStateToProps = state => {
   return {
     checkedEmail: state.checkedEmail,
-    checkedPassword: state.checkedPassword,
-  }
+    checkedPassword: state.checkedPassword
+  };
 };
 
 export default connect(
   mapStateToProps,
-  {setPath, checkEmail, checkPassword}
+  { setPath, checkEmail, checkPassword }
 )(Login);
--- Express/musicseed_react/src/pages/Register/Register.js
+++ Express/musicseed_react/src/pages/Register/Register.js
@@ -1,94 +1,95 @@
-import React, {Component} from 'react';
-import { connect } from 'react-redux';
-import { checkEmail, checkPassword, confirmPassword, checkName, checkNickname } from "../../actions/index";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import {
+  checkEmail,
+  checkPassword,
+  confirmPassword,
+  checkName,
+  checkNickname
+} from "../../actions/index";
 import axios from "axios";
 
 class Register extends Component {
-
-  confirmPasswordOnChange(){
-    if(this.props.checkedPassword === this.props.confirmedPassword) {
-        // console.log('OK');
-        return true;
-    }else{
-        return false;
+  confirmPasswordOnChange() {
+    if (this.props.checkedPassword === this.props.confirmedPassword) {
+      // console.log('OK');
     }
   }
 
-  componentDidUpdate(){
-    this.confirmPasswordOnChange();
+  componentDidUpdate() {
+    
   }
 
-
-    onFormSubmit = async (event) => {
-        event.preventDefault();
-        if(this.confirmPasswordOnChange()){
-            let user = {
-                email: this.props.checkedEmail,
-                pw: this.props.checkedPassword,
-                name: this.props.checkedName,
-                nickname: this.props.checkedNickname,
-            };
-            await axios.post('/api/users', user)
-                .then(function (response) {
-                    if (response.data.redirect === '/login') {
-                        window.location = "/login";
-                    } else{
-                        window.location = '/register';
-                    }
-                });
-        }
-    };
+  onFormSubmit = async event => {
+    event.preventDefault();
+    // if (this.confirmPasswordOnChange()) {
+    //   let user = {
+    //     email: this.props.checkedEmail,
+    //     pw: this.props.checkedPassword,
+    //     name: this.props.checkedName,
+    //     nickname: this.props.checkedNickname
+    //   };
+    //   await axios.post("/api/users", user).then(function(response) {
+    //     if (response.data.redirect === "/login") {
+    //       window.location = "/login";
+    //     } else {
+    //       window.location = "/register";
+    //     }
+    //   });
+    // }
+  };
 
   render() {
     return (
-      <form className="ui form" onSubmit={e => this.onFormSubmit(e) }>
+      <form className="ui form" onSubmit={e => this.onFormSubmit(e)}>
         <div className="field">
           <h1>Register</h1>
           <input
             type="email"
             placeholder="Email"
-            onChange={ e => this.props.checkEmail(e.target.value) }
-            value={ this.props.checkedEmail }
+            onChange={e => this.props.checkEmail(e.target.value)}
+            value={this.props.checkedEmail}
           />
         </div>
         <div className="field">
           <input
             type="password"
             placeholder="Password"
-            onChange={ e => this.props.checkPassword(e.target.value) }
-            value={ this.props.checkedPassword }
+            onChange={e => this.props.checkPassword(e.target.value)}
+            value={this.props.checkedPassword}
           />
         </div>
         <div className="field">
           <input
             type="password"
             placeholder="Password Confirm"
-            onChange={ e => this.props.confirmPassword(e.target.value) }
-            value={ this.props.confirmedPassword }
+            onChange={e => this.props.confirmPassword(e.target.value)}
+            value={this.props.confirmedPassword}
           />
         </div>
         <div className="field">
           <input
             type="text"
             placeholder="Name"
-            onChange={ e => this.props.checkName(e.target.value) }
-            value={ this.props.checkedName }
+            onChange={e => this.props.checkName(e.target.value)}
+            value={this.props.checkedName}
           />
         </div>
         <div className="field">
           <input
             type="text"
             placeholder="Nickname"
-            onChange={ e => this.props.checkNickname(e.target.value) }
-            value={ this.props.checkedNickname }
+            onChange={e => this.props.checkNickname(e.target.value)}
+            value={this.props.checkedNickname}
           />
         </div>
-        <button className="ui button" type="submit">REGISTER</button>
+        <button className="ui button" type="submit">
+          REGISTER
+        </button>
       </form>
     );
   }
 }
-
 
 const mapStateToProps = state => {
   return {
@@ -97,7 +98,7 @@ const mapStateToProps = state => {
     confirmedPassword: state.confirmedPassword,
     checkedName: state.checkedName,
     checkedNickname: state.checkedNickname
-  }
+  };
 };
 
 export default connect(
